09d79c87b9623e35a58dd881c6275d3b808e1120
shishir58 <shishir58.bd@gmail.com>
2015-05-13 16:05:46 +0600

    New File

---
+++ EmployeeInfoWithDataBase/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace EmployeeInfoWithDataBase
+{
+    partial class employeeInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.employeeListView = new System.Windows.Forms.ListView();
+            this.idHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.nameHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.addressHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.emailHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.salaryHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(140, 225);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Salary";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(140, 46);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(167, 20);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(140, 86);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(167, 20);
+            this.addressTextBox.TabIndex = 2;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(140, 125);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(167, 20);
+            this.emailTextBox.TabIndex = 2;
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(140, 166);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(167, 20);
+            this.salaryTextBox.TabIndex = 2;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(232, 225);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 3;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // employeeListView
+            // 
+            this.employeeListView.BackgroundImageTiled = true;
+            this.employeeListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.idHeader,
+            this.nameHeader,
+            this.addressHeader,
+            this.emailHeader,
+            this.salaryHeader});
+            this.employeeListView.FullRowSelect = true;
+            this.employeeListView.Location = new System.Drawing.Point(352, 46);
+            this.employeeListView.Name = "employeeListView";
+            this.employeeListView.Size = new System.Drawing.Size(366, 233);
+            this.employeeListView.TabIndex = 4;
+            this.employeeListView.UseCompatibleStateImageBehavior = false;
+            this.employeeListView.View = System.Windows.Forms.View.Details;
+            this.employeeListView.DoubleClick += new System.EventHandler(this.employeeListView_DoubleClick);
+            // 
+            // idHeader
+            // 
+            this.idHeader.Text = "ID";
+            // 
+            // nameHeader
+            // 
+            this.nameHeader.Text = "Name";
+            // 
+            // addressHeader
+            // 
+            this.addressHeader.Text = "Address";
+            // 
+            // emailHeader
+            // 
+            this.emailHeader.Text = "Email";
+            // 
+            // salaryHeader
+            // 
+            this.salaryHeader.Text = "Salary";
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(140, 281);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 5;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(232, 281);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // employeeInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 459);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.employeeListView);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Name = "employeeInfoForm";
+            this.Text = "Emplouee Information UI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ListView employeeListView;
+        private System.Windows.Forms.ColumnHeader idHeader;
+        private System.Windows.Forms.ColumnHeader nameHeader;
+        private System.Windows.Forms.ColumnHeader addressHeader;
+        private System.Windows.Forms.ColumnHeader emailHeader;
+        private System.Windows.Forms.ColumnHeader salaryHeader;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ EmployeeInfoWithDataBase/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeInfoWithDataBase
+{
+    public partial class employeeInfoForm : Form
+    {
+        public employeeInfoForm()
+        {
+            InitializeComponent();
+        }
+
+        private Employee anEmployee = new Employee();
+        private int rowAffected;
+        private List<Employee> myList = new List<Employee>();
+        private bool isUpdateMode = false;
+        private int employeeId;
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            anEmployee.name = nameTextBox.Text;
+            anEmployee.address = addressTextBox.Text;
+            anEmployee.email = emailTextBox.Text;
+            anEmployee.salary = Convert.ToDouble(salaryTextBox.Text);
+            if (IfEmailExist(anEmployee.email))
+            {
+                MessageBox.Show("Email already Exist!");
+                return;
+            }
+            string connectionString = @"Server=PC-301-11\SQLEXPRESS;Database=EmployeeInfoDB;Trusted_Connection=True;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            String query = "INSERT INTO employeeTB VALUES ('" + anEmployee.name + "','" + anEmployee.address + "','" +
+                           anEmployee.email + "','" + anEmployee.salary + "');";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            rowAffected = cmd.ExecuteNonQuery();
+            connection.Close();
+            if (rowAffected > 0)
+            {
+                MessageBox.Show("Updated Successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Update Failed!");
+            }
+        }
+
+        public bool IfEmailExist(String email)
+        {
+            string connectionString = @"Server=PC-301-11\SQLEXPRESS;Database=EmployeeInfoDB;Trusted_Connection=True;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            String query = "SELECT * FROM employeeTB WHERE email='" + email + "'";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            bool IfEmailExist = false;
+            while (reader.Read())
+            {
+                IfEmailExist = true;
+                break;
+            }
+            reader.Close();
+            connection.Close();
+            return IfEmailExist;
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            ShowAllEmployee();
+        }
+
+        public void LoadEmployeeListView(List<Employee> employees)
+        {
+            employeeListView.Items.Clear();
+            foreach (var employee in employees)
+            {
+                ListViewItem item = new ListViewItem(employee.id.ToString());
+                item.SubItems.Add(employee.name);
+                item.SubItems.Add(employee.address);
+                item.SubItems.Add(employee.email);
+                item.SubItems.Add(employee.salary.ToString());
+                employeeListView.Items.Add(item);
+            }
+        }
+
+        public void ShowAllEmployee()
+        {
+            string connectionString = @"Server=PC-301-11\SQLEXPRESS;Database=EmployeeInfoDB;Trusted_Connection=True;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            String query = "SELECT * FROM employeeTB";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Employee> myList = new List<Employee>();
+            while (reader.Read())
+            {
+
+                Employee anEmployee = new Employee();
+                anEmployee.id = int.Parse(reader["ID"].ToString());
+                anEmployee.name = reader["Name"].ToString();
+                anEmployee.address = reader["Address"].ToString();
+                anEmployee.email = reader["Email"].ToString();
+                anEmployee.salary = Convert.ToDouble(reader["Salary"].ToString());
+
+                myList.Add(anEmployee);
+            }
+            reader.Close();
+            connection.Close();
+            LoadEmployeeListView(myList);
+        }
+
+        private void employeeListView_DoubleClick(object sender, EventArgs e)
+        {
+            ListViewItem item = employeeListView.SelectedItems[0];
+
+            int id = int.Parse(item.Text.ToString());
+
+            Employee anEmployee = GetEmployeeByID(id);
+
+            if (anEmployee != null)
+            {
+                isUpdateMode = true;
+                //saveButton.Text = "Update";
+                employeeId = anEmployee.id;
+                nameTextBox.Text = item.SubItems[1].Text;
+                addressTextBox.Text = item.SubItems[2].Text;
+                emailTextBox.Text = item.SubItems[3].Text;
+                salaryTextBox.Text = item.SubItems[4].Text;
+            }
+        }
+
+        public Employee GetEmployeeByID(int id)
+        {
+            string connectionString = @"Server=PC-301-11\SQLEXPRESS;Database=EmployeeInfoDB;Trusted_Connection=True;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "SELECT * FROM employeeTB WHERE ID ='" + id + "'";
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Employee> myList = new List<Employee>();
+
+            while (reader.Read())
+            {
+
+                Employee anEmployee = new Employee();
+                anEmployee.id = int.Parse(reader["ID"].ToString());
+                anEmployee.name = reader["Name"].ToString();
+                anEmployee.address = reader["Address"].ToString();
+                anEmployee.email = reader["Email"].ToString();
+                anEmployee.salary = Convert.ToDouble(reader["Salary"].ToString());
+                myList.Add(anEmployee);
+            }
+            reader.Close();
+            connection.Close();
+            return myList.FirstOrDefault();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            anEmployee.name = nameTextBox.Text;
+            anEmployee.address = addressTextBox.Text;
+            anEmployee.email = emailTextBox.Text;
+            anEmployee.salary = Convert.ToDouble(salaryTextBox.Text);
+            if (isUpdateMode)
+            {
+                string connectionString =
+                    @"Server=PC-301-11\SQLEXPRESS;Database=EmployeeInfoDB;Trusted_Connection=True;";
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                string query = "UPDATE employeeTB SET Name ='" + anEmployee.name + "', Address ='" + anEmployee.address +
+                               "', Email ='" + anEmployee.email + "', Salary ='" + anEmployee.salary + "' WHERE ID = '" +
+                               employeeId + "'";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                int rowAffected = command.ExecuteNonQuery();
+                connection.Close();
+                if (rowAffected > 0)
+                {
+                    MessageBox.Show("Updated Successfully!");
+                    saveButton.Text = "Save";
+                    employeeId = 0;
+                    isUpdateMode = false;
+                    ShowAllEmployee();
+                }
+                else
+                {
+                    MessageBox.Show("Update Failed!");
+                }
+            }
+
+            else
+            {
+                if (IfEmailExist(anEmployee.email))
+                {
+                    MessageBox.Show("Email already Exist!");
+                    return;
+                }
+                string connectionString = @"Server=PC-301-11\SQLEXPRESS;Database=EmployeeInfoDB;Trusted_Connection=True;";
+
+                SqlConnection connection = new SqlConnection(connectionString);
+                String query = "INSERT INTO employeeTB VALUES ('" + anEmployee.name + "','" + anEmployee.address + "','" +
+                               anEmployee.email + "','" + anEmployee.salary + "');";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                connection.Open();
+                rowAffected = cmd.ExecuteNonQuery();
+                connection.Close();
+                if (rowAffected > 0)
+                {
+                    MessageBox.Show("Updated Successfully!");
+                }
+                else
+                {
+                    MessageBox.Show("Update Failed!");
+                }
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = employeeListView.SelectedItems[0];
+
+            int id = int.Parse(item.Text.ToString());
+
+            //Employee anEmployee = GetEmployeeByID(id);
+
+            if (anEmployee != null)
+            {
+                string connectionString = @"Server=PC-301-11\SQLEXPRESS;Database=EmployeeInfoDB;Trusted_Connection=True;";
+                SqlConnection connection = new SqlConnection(connectionString);
+                String query = "Select * From employeeTB WHERE ID ='" + anEmployee.id+ "'";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                connection.Open();
+                rowAffected = cmd.ExecuteNonQuery();
+                connection.Close();
+                if (rowAffected > 0)
+                {
+                    MessageBox.Show("Delete Successfully!");
+                }
+                else
+                {
+                    MessageBox.Show("Delete Failed!");
+                }
+            }
+
+        }
+    }
+}
